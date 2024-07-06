742a6c91cecd3e80a66f9b48364121bd9d3f1c55
DevWedeloper <vicnathangabrielle@gmail.com>
2024-07-06 09:10:10 +0800

    refactor: remove initial example code

--- src/app/pages/(home).page.ts
+++ src/app/pages/(home).page.ts
@@ -1,14 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { AnalogWelcomeComponent } from './analog-welcome.component';
-
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AnalogWelcomeComponent],
-  template: `
-    <app-analog-welcome />
-  `,
+  imports: [],
+  template: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class HomeComponent {}
