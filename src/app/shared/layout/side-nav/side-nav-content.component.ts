602e3e3c2bc3f72ee30c6b66c5904205e92ef983
DevWedeloper <vicnathangabrielle@gmail.com>
2024-07-08 17:27:26 +0800

    feat: display prompt titles

--- src/app/shared/layout/side-nav/side-nav-content.component.ts
+++ src/app/shared/layout/side-nav/side-nav-content.component.ts
@@ -1,15 +1,44 @@
-import { ChangeDetectionStrategy, Component, output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  output,
+} from '@angular/core';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { GeminiService } from '../../gemini.service';
 
 @Component({
   selector: 'app-side-nav-content',
   standalone: true,
-  imports: [],
+  imports: [HlmButtonDirective],
+  host: {
+    class: 'flex flex-col',
+  },
   template: `
-    <p>side-nav-content works!</p>
+    @for (history of promptHistory(); track $index) {
+      <button
+        hlmBtn
+        variant="link"
+        class="flex justify-start p-2 hover:bg-accent hover:no-underline"
+        (click)="handleClick(history.id)"
+      >
+        {{ history.title }}
+      </button>
+    } @empty {
+      <p>No messages yet.</p>
+    }
   `,
-  styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SideNavContentComponent {
   promptClicked = output();
+
+  private geminiService = inject(GeminiService);
+  protected promptHistory = this.geminiService.promptHistory;
+  private selectedPromptId = this.geminiService.selectedPromptId;
+
+  protected handleClick(id: number): void {
+    this.selectedPromptId.set(id);
+    this.promptClicked.emit();
+  }
 }
