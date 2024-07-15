8d8312b9c304cf1f06d73d783f079519d641f5ca
DevWedeloper <vicnathangabrielle@gmail.com>
2024-07-15 16:17:37 +0800

    fix: remove extra slash

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  api: 'https://gemini-clone-devwedeloper.vercel.app/',
+  api: 'https://gemini-clone-devwedeloper.vercel.app',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  api: 'http://localhost:5173/api/',
+  api: 'http://localhost:5173/api',
 };
