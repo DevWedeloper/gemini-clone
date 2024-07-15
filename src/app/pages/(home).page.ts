949d9f10302b5cbede77f96853c2597ab3eb2b49
DevWedeloper <vicnathangabrielle@gmail.com>
2024-07-15 09:52:52 +0800

    feat: update meta tags

--- src/app/pages/(home).page.ts
+++ src/app/pages/(home).page.ts
@@ -1,6 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { RouteMeta } from '@analogjs/router';
+import { metaWith } from '../shared/meta/meta.util';
 import { GeminiComponent } from './gemini.component';
+
+export const routeMeta: RouteMeta = {
+  meta: metaWith(
+    'Gemini Clone - Home',
+    'Access and interact with the Gemini API.',
+  ),
+  title: 'Gemini Clone - Home',
+};
 
 @Component({
   selector: 'app-home',
---
+++ src/app/shared/meta/meta.util.ts
@@ -0,0 +1,51 @@
+export const metaWith = (title: string, description: string) => [
+  {
+    name: 'description',
+    content: description,
+  },
+  {
+    name: 'author',
+    content: 'DevWedeloper',
+  },
+  {
+    property: 'og:title',
+    content: title,
+  },
+  {
+    property: 'og:site_name',
+    content: 'Angular Form Builder',
+  },
+  {
+    property: 'og:type',
+    content: 'website',
+  },
+  {
+    property: 'og:url',
+    content: 'https://gemini-clone-devwedeloper.vercel.app/',
+  },
+  {
+    property: 'og:description',
+    content: description,
+  },
+  {
+    property: 'og:image',
+    content: 'https://gemini-clone-devwedeloper.vercel.app/favicon.ico',
+  },
+
+  {
+    property: 'twitter:card',
+    content: 'summary_large_image',
+  },
+  {
+    property: 'twitter:title',
+    content: title,
+  },
+  {
+    property: 'twitter:description',
+    content: description,
+  },
+  {
+    property: 'twitter:image',
+    content: 'https://gemini-clone-devwedeloper.vercel.app/favicon.ico',
+  },
+];
